5d15741bc343f2066825e64dda4f248674c098e1
Melih Koçak <melih3660k@gmail.com>
2025-12-09 14:25:52 +0300

    CreateProductRequestValidator Eklendi.

---
+++ App.Services/Products/CreateProductRequestDTOValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Services.Products
+{
+    public class CreateProductRequestDTOValidator:AbstractValidator<CreateProductRequestDto>
+    {
+        public CreateProductRequestDTOValidator()
+        {
+            RuleFor(x => x.name)
+                .NotNull().WithMessage("Ürün ismi null olamaz.")
+                .NotEmpty().WithMessage("Ürün ismi boş olamaz.")
+                .Length(3,10).WithMessage("Ürün İsmi 3 ile 10 karakter arasında olmalıdır.");
+
+            RuleFor(x => x.price)
+               .GreaterThan(0).WithMessage("Ürün Fiyatı Sıfırdan Büyük Olmalıdır.");
+
+            RuleFor(x => x.stock)
+               .InclusiveBetween(1, 500).WithMessage("Ürün Stock Adeti 1 İle 500 Arasında Olmalıdır.");
+
+
+        }
+    }
+}
