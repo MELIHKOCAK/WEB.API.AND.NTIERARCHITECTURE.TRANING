694f681bd225db908c1b69b6688998b5b9b4e766
Melih Koçak <melih3660k@gmail.com>
2025-12-16 14:13:54 +0300

    • Controllerde servis metotların hepsinin endpoint karşılığı yazıldı.

--- App.Api/Controller/CateogriesController.cs
+++ App.Api/Controller/CateogriesController.cs
@@ -11,7 +11,33 @@ namespace App.Api.Controller
         public async Task<IActionResult> Create(CreateCategoryRequestDto requestDto) =>
             CreateActionResult(await service.CreateAsync(requestDto));
 
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+    CreateActionResult(await service.GetAllAsync());
 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCategory([FromRoute] int id) =>
+            CreateActionResult(await service.GetByIdAsync(id));
+
+        [HttpGet("{id:int}/products")]
+        public async Task<IActionResult> GetCategoryByIdWithProducts([FromRoute] int id) =>
+            CreateActionResult(await service.GetCategoryByIdWithProductAsync(id));
+
+        [HttpGet("{pageSize:int}/{pageNumber:int}")]
+        public async Task<IActionResult> GetCategoryByIdWithProducts([FromRoute] int pageSize, [FromRoute] int pageNumber) =>
+          CreateActionResult(await service.GetPagedAllListAsync(pageNumber, pageSize));
+
+        [HttpGet("/products")]
+        public async Task<IActionResult> GetCategoryWithProducts() =>
+           CreateActionResult(await service.GetCategoryAllWithProductAsync());
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory([FromBody] UpdateCategoryRequestDto requestDto) =>
+            CreateActionResult(await service.UpdateAsync(requestDto));
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteCategory([FromRoute] int id) =>
+            CreateActionResult(await service.DeleteAsync(id));
 
     }
 }
